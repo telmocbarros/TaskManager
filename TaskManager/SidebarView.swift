5ddbc33bfb5004f6842fe5124c4572e8ededd1cb
Telmo Barros <somebodyceo@gmail.com>
2025-02-25 19:39:40 +0100

    wip: side bar view

--- TaskManager/SidebarView.swift
+++ TaskManager/SidebarView.swift
@@ -5,3 +5,35 @@
 //  Created by Telmo Barros on 25/02/2025.
 //
 
+import SwiftUI
+
+struct SidebarView: View {
+    let userCreatedGroups: [TaskGroup]
+    @Binding var selection: TaskSection
+    
+    var body: some View {
+        List(selection: $selection) {
+            Section("Favourites") {
+                ForEach(TaskSection.allCases) { selection in
+                    Label(
+                        selection.displayName,
+                        systemImage: selection.iconName
+                    ).tag(selection)
+                }
+            }
+            
+            Section("Your Groups") {
+                ForEach(userCreatedGroups){group in
+                    Label(group.title, systemImage: "folder")
+                        .tag(TaskSection.list(group))
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SidebarView(userCreatedGroups: TaskGroup.examples(), selection:
+            .constant(.all))
+    .listStyle(.sidebar)
+}
