c1b73c9456b2bad74fbe2ebe35a2a19ced5ad644
Telmo Barros <somebodyceo@gmail.com>
2025-02-25 19:40:02 +0100

    wip: content view

--- TaskManager/ContentView.swift
+++ TaskManager/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection = TaskSection.all
+    @State private var allTasks = Task.examples()
+    @State private var userCreatedGroups: [TaskGroup] = TaskGroup.examples()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationSplitView {
+            SidebarView(userCreatedGroups: userCreatedGroups, selection: $selection)
+        } detail: {
+            switch selection {
+            case .all:
+                TaskListView(title: "All", tasks: allTasks)
+            case .done:
+                TaskListView(title: "All", tasks: allTasks.filter({$0.isCompleted}))
+            case .upcoming:
+                TaskListView(title: "All", tasks: allTasks.filter({!$0.isCompleted}))
+                
+            case .list(let taskGroup):
+                TaskListView(title: taskGroup.title, tasks: taskGroup.tasks)
+            }
         }
-        .padding()
     }
 }
 
