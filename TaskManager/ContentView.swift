58ee592125cefc79595d1948aea2ced5d5320860
Telmo Barros <somebodyceo@gmail.com>
2025-02-27 20:01:14 +0100

    introduce filter

--- TaskManager/ContentView.swift
+++ TaskManager/ContentView.swift
@@ -8,25 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selection = TaskSection.all
+    @State private var selection: TaskSection? = TaskSection.all
     @State private var allTasks = Task.examples()
     @State private var userCreatedGroups: [TaskGroup] = TaskGroup.examples()
+    @State private var searchTerm: String = ""
+    
     var body: some View {
         NavigationSplitView {
-            SidebarView(userCreatedGroups: $userCreatedGroups, selection: $selection)
+            SidebarView(
+                userCreatedGroups: $userCreatedGroups,
+                selection: $selection
+            )
         } detail: {
-            switch selection {
-            case .all:
-                TaskListView(title: "All", tasks: allTasks)
-            case .done:
-                TaskListView(title: "All", tasks: allTasks.filter({$0.isCompleted}))
-            case .upcoming:
-                TaskListView(title: "All", tasks: allTasks.filter({!$0.isCompleted}))
-                
-            case .list(let taskGroup):
-                TaskListView(title: taskGroup.title, tasks: taskGroup.tasks)
+            if (searchTerm.isEmpty){
+                switch selection {
+                    case .all:
+                        TaskListView(title: "All", tasks: $allTasks)
+                    case .done:
+                        StaticTaskListView(
+                            title: "All",
+                            tasks: allTasks.filter({ $0.isCompleted })
+                        )
+                    case .upcoming:
+                        StaticTaskListView(
+                            title: "All",
+                            tasks: allTasks.filter({ !$0.isCompleted })
+                        )
+                        
+                    case .list(let taskGroup):
+                        StaticTaskListView(title: taskGroup.title, tasks: taskGroup.tasks)
+                    case .none:
+                        Text("none")
+                }
+            } else {
+                StaticTaskListView(title: "All", tasks: allTasks.filter({ $0.title.contains(searchTerm) }))
             }
-        }
+        }.searchable(text: $searchTerm)
     }
 }
 
